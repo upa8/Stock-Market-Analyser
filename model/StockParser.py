09ebccf768254870a2867467e1b9710dc98e9638
upa8 <pratikupacharya@gmail.com>
2016-12-01 12:54:07 +0530

    Parsed data from url

--- model/StockParser.py
+++ model/StockParser.py
@@ -1,14 +1,15 @@
-from bs4 import BeautifulSoup
+import urllib2
+import json
 
-import requests
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
-
-url = "https://www.nseindia.com/live_market/dynaContent/live_analysis/top_gainers_losers.htm?cat=G"
-
-r  = requests.get(url)
-
-data = r.text
-
-soup = BeautifulSoup(data)
-
-print soup
+req = urllib2.Request("https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json",headers=hdr)
+opener = urllib2.build_opener()
+f = opener.open(req)
+json = json.loads(f.read())
+print json['time']
